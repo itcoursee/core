2b8a12ffb588627129a6bb909887da2cdb4eb4a0
storezhang <storezhang@gmail.com>
2024-01-19 13:31:02 +0800

    fix(分类): 增加分类和子分类限制

---
+++ kernel/category.go
@@ -0,0 +1,37 @@
+package kernel
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/itcoursee/core/internal/callback"
+	"github.com/itcoursee/core/internal/constant"
+)
+
+type Category string
+
+func (c *Category) Case(direct callback.Category, children callback.Category, subcategory callback.Category) {
+	if "" == strings.TrimSpace(string(*c)) || 0 >= c.id() {
+		return
+	}
+
+	id := c.id()
+	switch string(strings.TrimSpace(string(*c))[0]) {
+	case constant.DescendantTypeDirect:
+		direct(id)
+	case constant.DescendantTypeChildren:
+		children(id)
+	case constant.DescendantTypeSubcategory:
+		subcategory(id)
+	}
+}
+
+func (c *Category) id() (id int64) {
+	if value, pie := strconv.ParseInt(string((*c)[1:]), 10, 64); nil == pie {
+		id = value
+	} else {
+		id = -1
+	}
+
+	return
+}
